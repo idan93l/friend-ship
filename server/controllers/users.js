91bf8060922cad1fbf296bba4c9d8a7155e5e2b2
Idan Lev <idan.93lev@gmail.com>
2023-01-31 23:24:41 +0200

    finished read functions in user controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,30 @@ import User from "../models/User";
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).send(user);
   } catch (err) {
     res.status(404).json({ msg: err.message });
   }
 };
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
+
+/* UPDATE */
