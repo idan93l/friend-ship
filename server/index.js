0622ebe4e49b9eb357109d9cfb801644c33e0d37
Idan Lev <idan.93lev@gmail.com>
2023-02-01 11:42:29 +0200

    start working on posts routes and controllers

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js"
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,11 +42,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.use("auth/register", upload.single("picture", register));
+app.use("auth/register", upload.single("picture"), register);
+app.use("/posts", verifyToken, upload.single("picture"), createPost)
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts",postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
