6eb2adfc5a32f8928a483f82fc4dc08c45276e31
Idan Lev <idan.93lev@gmail.com>
2023-02-02 18:50:56 +0200

    finished likePost function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -44,3 +44,28 @@ export const getUserPosts = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+/* Update */
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.find(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.like.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
