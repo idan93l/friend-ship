2fd224b461ef9b1d114b62aa2a5fec1a47b135f2
Idan Lev <idan.93lev@gmail.com>
2023-02-02 21:09:23 +0200

    minor changes in index.js

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js"
+import { createPost } from "./controllers/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js"
-import Post from "./models/Post.js"
-import { users, posts } from "./data/index.js"
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -46,12 +46,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.use("auth/register", upload.single("picture"), register);
-app.use("/posts", verifyToken, upload.single("picture"), createPost)
+app.use("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/posts",postRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
