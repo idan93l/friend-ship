e71675e6cc22b73c118076e13c2626e694144458
Idan Lev <idan.93lev@gmail.com>
2023-02-04 14:57:15 +0200

    added variables to navbar components

--- client/src/scenes/navbar/index.js
+++ client/src/scenes/navbar/index.js
@@ -26,6 +26,21 @@ import { useNavigate } from "react-router-dom";
 import FlexBetween from "components/FlexBetween";
 
 const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
   return <div>Navbar</div>;
 };
 
