9d1f632352ebd4a14f9c33544536711e904f8f27
Idan Lev <idan.93lev@gmail.com>
2023-02-05 13:42:29 +0200

    added in Form.jsx - formik email textField

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -163,6 +163,16 @@ const Form = () => {
                 </Box>
               </>
             )}
+            <TextField
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={Boolean(touched.email) && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: "span 2" }}
+            />
           </Box>
         </form>
       )}
