0b2f4c7b783774982cf0fd2d6e06c84a11397cac
Idan Lev <idan.93lev@gmail.com>
2023-02-05 23:50:01 +0200

    finished second row in UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -78,6 +78,20 @@ const userWidget = ({ userId, picturePath }) => {
         <Divider />
 
         {/* SECOND ROW */}
+        <Box p="1rem 0">
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.rem">
+            <LocationOnOUtlined fontSize="large" sx={{color: main}} /> 
+            <Typography color={medium}>{location}</Typography>
+          </Box>
+          <Box display="flex" alignItems="center" gap="1rem" >
+            <WorkOutlineOutlined fontSize="large" sx={{color: main}} /> 
+            <Typography color={medium}>{occupation}</Typography>
+          </Box>
+        </Box>
+
+        {/* THIRD ROW */}
+        
+
       </FlexBetween>
     </WidgetWrapper>
   );
