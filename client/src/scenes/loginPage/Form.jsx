92832ba77a3e546034840d20e9d630ebdf66693c
Idan Lev <idan.93lev@gmail.com>
2023-02-05 13:54:00 +0200

    added in Form.jsx - buttons

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -185,6 +185,40 @@ const Form = () => {
               sx={{ gridColumn: "span 2" }}
             />
           </Box>
+          {/* BUTTONS */}
+          <Box>
+            <Button
+              fullWidth
+              type="submit"
+              sx={{
+                m: "2rem 0",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              {isLogin ? "LOGIN" : "REGISTER"}
+            </Button>
+            <Typography
+              onClick={() => {
+                setPageType(isLogin ? "register" : "login");
+                resetForm();
+              }}
+              sx={{
+                textDecoration: "underline",
+                color: palette.primary.main,
+                "&:hover": {
+                  cursor: "pointer",
+                  color: palette.primary.light,
+                },
+              }}
+            >
+              {isLogin
+                ? "Don't have an account? Sign up here."
+                : "Already have an account? Login here."}
+            </Typography>
+          </Box>
         </form>
       )}
     </Formik>
