bba7e4b8696575576d4086f3c5ab94c313758c5c
Idan Lev <idan.93lev@gmail.com>
2023-02-05 23:26:53 +0200

    added getUser function in UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -3,23 +3,32 @@ import {
   EditOutlined,
   LocationOnOUtlined,
   WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "components/userImageWidget";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-} from "@mui/icons-material"
-import { Box, Typography, Divider, useTheme } from "@mui/material"
-import UserImage from "components/userImageWidget"
-import FlexBetween from "components/FlexBetween"
-import WidgetWrapper from "components/WidgetWrapper"
-import { useSelector } from "react-redux"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-
-const userWidget = ({userId, picturePath}) => {
+const userWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
   const { palette } = useTheme();
   const navigate = useNavigate();
-  const toekn = useSelector((state) => state.token)
+  const token = useSelector((state) => state.token);
   const dark = palette.neutral.dark;
   const medium = palette.neutral.medium;
   const main = palette.neutral.main;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
   
-}
+};
