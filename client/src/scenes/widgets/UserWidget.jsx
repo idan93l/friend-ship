7e69f0f42c2aa4e85673e881451bb2ba0f2c11b0
Idan Lev <idan.93lev@gmail.com>
2023-02-06 15:55:40 +0200

    finished UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -106,7 +106,37 @@ const userWidget = ({ userId, picturePath }) => {
         </Box>
 
         {/* FOURTH ROW */}
-        
+        <Box p="1rem">
+            <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+              Social Profiles
+            </Typography>
+
+            <FlexBetween gap="1rem" mb="0.5rem">
+              <FlexBetween gap="1rem">
+                <img src="../assets/twitter.png" alt="twitter" />
+                <Box>
+                  <Typography color={main} fontWeight="500">
+                    Twitter
+                  </Typography>
+                  <Typography color={medium}>Social Network</Typography>
+                </Box>
+              </FlexBetween>
+              <EditOutlined sx={{ color: main }} />
+            </FlexBetween>
+
+            <FlexBetween gap="1rem">
+              <FlexBetween gap="1rem">
+                <img src="../assets/linkedin.png" alt="linkedin" />
+                <Box>
+                  <Typography color={main} fontWeight="500">
+                    LinkedIn
+                  </Typography>
+                  <Typography color={medium}>Network Platform</Typography>
+                </Box>
+              </FlexBetween>
+              <EditOutlined sx={{ color: main }} />
+            </FlexBetween>
+        </Box>
       </FlexBetween>
     </WidgetWrapper>
   );
