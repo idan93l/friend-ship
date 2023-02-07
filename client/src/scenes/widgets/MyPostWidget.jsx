984effdefd4824d8bf8825f5414996bc36fbd1be
Idan Lev <idan.93lev@gmail.com>
2023-02-07 22:21:44 +0200

    added variables in MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -27,7 +27,16 @@ import { setPosts } from "state";
 import { useTheme } from "@emotion/react";
 
 const MyPostWidget = ({ PicturePath }) => {
-
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, SetPost] = useState("");
+  const { palette } = useTheme();
+  const {_id} = useSelector((state) => state.user);
+  const { token } = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
 };
 
 export default MyPostWidget;
