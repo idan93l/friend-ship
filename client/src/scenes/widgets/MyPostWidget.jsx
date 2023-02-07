50f65dcb83aea987e947f95cd4f8a7f7c3eb171d
Idan Lev <idan.93lev@gmail.com>
2023-02-07 22:21:03 +0200

    started working on MyPostWidgets imports

---
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,33 @@
+import {
+  EditOutlined,
+  deleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Typography,
+  InputBase,
+  UseTheme,
+  Button,
+  iconButton,
+  useMediaQuery,
+} from "@mui/material";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImageWidget";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import { useTheme } from "@emotion/react";
+
+const MyPostWidget = ({ PicturePath }) => {
+
+};
+
+export default MyPostWidget;
