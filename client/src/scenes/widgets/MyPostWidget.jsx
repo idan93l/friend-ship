a39bd135a99b6d02a496b6d5bc0b81f0d8ff109b
Idan Lev <idan.93lev@gmail.com>
2023-02-07 22:41:04 +0200

    added handlePost function in MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -30,13 +30,34 @@ const MyPostWidget = ({ PicturePath }) => {
   const dispatch = useDispatch();
   const [isImage, setIsImage] = useState(false);
   const [image, setImage] = useState(null);
-  const [post, SetPost] = useState("");
+  const [post, setPost] = useState("");
   const { palette } = useTheme();
-  const {_id} = useSelector((state) => state.user);
+  const { _id } = useSelector((state) => state.user);
   const { token } = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const mediumMain = palette.neutral.mediumMain;
   const medium = palette.neutral.medium;
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    }
+
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
 };
 
 export default MyPostWidget;
