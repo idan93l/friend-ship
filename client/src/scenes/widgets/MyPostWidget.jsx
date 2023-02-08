ca44a51c61476da122402ab49f6b6f01a8f31b61
Idan Lev <idan.93lev@gmail.com>
2023-02-08 14:25:37 +0200

    few typos fixed in MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,42 +1,39 @@
 import {
   EditOutlined,
-  deleteOutlined,
+  DeleteOutlined,
   AttachFileOutlined,
   GifBoxOutlined,
   ImageOutlined,
   MicOutlined,
   MoreHorizOutlined,
-  DeleteOutlined,
 } from "@mui/icons-material";
 import {
   Box,
   Divider,
   Typography,
   InputBase,
-  UseTheme,
+  useTheme,
   Button,
-  iconButton,
-  useMediaQuery,
   IconButton,
+  useMediaQuery,
 } from "@mui/material";
-import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
+import Dropzone from "react-dropzone";
 import UserImage from "components/UserImageWidget";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
-import { useTheme } from "@emotion/react";
 
-const MyPostWidget = ({ PicturePath }) => {
+const MyPostWidget = ({ picturePath }) => {
   const dispatch = useDispatch();
   const [isImage, setIsImage] = useState(false);
   const [image, setImage] = useState(null);
   const [post, setPost] = useState("");
   const { palette } = useTheme();
   const { _id } = useSelector((state) => state.user);
-  const { token } = useSelector((state) => state.token);
-  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const mediumMain = palette.neutral.mediumMain;
   const medium = palette.neutral.medium;
 
@@ -54,7 +51,6 @@ const MyPostWidget = ({ PicturePath }) => {
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
     });
-
     const posts = await response.json();
     dispatch(setPosts({ posts }));
     setImage(null);
@@ -64,9 +60,9 @@ const MyPostWidget = ({ PicturePath }) => {
   return (
     <WidgetWrapper>
       <FlexBetween gap="1.5rem">
-        <UserImage image={PicturePath} />
+        <UserImage image={picturePath} />
         <InputBase
-          placeholder="What's on your mind?"
+          placeholder="What's on your mind..."
           onChange={(e) => setPost(e.target.value)}
           value={post}
           sx={{
@@ -100,7 +96,7 @@ const MyPostWidget = ({ PicturePath }) => {
                 >
                   <input {...getInputProps()} />
                   {!image ? (
-                    <p>Add image here</p>
+                    <p>Add Image Here</p>
                   ) : (
                     <FlexBetween>
                       <Typography>{image.name}</Typography>
@@ -134,6 +130,7 @@ const MyPostWidget = ({ PicturePath }) => {
             Image
           </Typography>
         </FlexBetween>
+
         {isNonMobileScreens ? (
           <>
             <FlexBetween gap="0.25rem">
@@ -157,13 +154,13 @@ const MyPostWidget = ({ PicturePath }) => {
           </FlexBetween>
         )}
 
-        <Button 
+        <Button
           disabled={!post}
           onClick={handlePost}
           sx={{
             color: palette.background.alt,
             backgroundColor: palette.primary.main,
-            borderRadius: "3rem"
+            borderRadius: "3rem",
           }}
         >
           POST
