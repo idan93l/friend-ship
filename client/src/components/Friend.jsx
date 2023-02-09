0ae36eed744b559f908105621a9e758839a7bf44
Idan Lev <idan.93lev@gmail.com>
2023-02-09 12:50:00 +0200

    added imports in Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -1,5 +1,9 @@
 import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImageWidget";
 
 const Friend = () => {
   return <div>Friend</div>;
