99ac2d0fa7d50655e895f48334ab9bac006f089a
Idan Lev <idan.93lev@gmail.com>
2023-02-11 20:27:44 +0200

    added variables from friend.jsx component to FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -1,6 +1,17 @@
 import { Typography, Box, useTheme } from "@mui/material"
+import Friend from "components/Friend"
+import WidgetWrapper from "components/WidgetWrapper"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setFriends } from "state"
 
-const FriendListWidget = () => {
+const FriendListWidget = ({userId}) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
   return (
     <div>FriendListWidget</div>
   )
