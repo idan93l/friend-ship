8908259db3bc65ab244ed6f5c9c09307495d32fe
Idan Lev <idan.93lev@gmail.com>
2023-02-11 23:51:30 +0200

    added varibles and states in profile

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,4 +1,4 @@
-import { Box, useMediaQuery, } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -8,8 +8,12 @@ import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
-
 const ProfilePage = () => {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
   return <div>ProfilePage</div>;
 };
 
