4b8927c3a22a95a04e5372ae37a433e02e4fcc32
Idan Lev <idan.93lev@gmail.com>
2023-02-11 23:55:14 +0200

    added getUser function in profile

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -14,6 +14,17 @@ const ProfilePage = () => {
   const token = useSelector((state) => state.token);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`,
+      {
+        methos: "GET",
+        header: { Authorization: `Bearer ${token}` }
+      }
+    )
+    const data = await response.json();
+    setUser(data);
+  }
+
   return <div>ProfilePage</div>;
 };
 
